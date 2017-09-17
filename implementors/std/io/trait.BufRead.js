0c3daca1205711ab224bf86146deaec0a41fd799
Michael Bryan <michaelfbryan@gmail.com>
2017-09-18 00:36:14 +0800

    updated documentation at 2017-09-18 00:36:14.264006013 +08:00

--- implementors/std/io/trait.BufRead.js
+++ implementors/std/io/trait.BufRead.js
@@ -1,5 +1,4 @@
 (function() {var implementors = {};
-implementors["flate2"] = ["impl&lt;R:&nbsp;<a class=\"trait\" href=\"std/io/trait.BufRead.html\" title=\"trait std::io::BufRead\">BufRead</a>&gt; <a class=\"trait\" href=\"std/io/trait.BufRead.html\" title=\"trait std::io::BufRead\">BufRead</a> for <a class=\"struct\" href=\"flate2/struct.CrcReader.html\" title=\"struct flate2::CrcReader\">CrcReader</a>&lt;R&gt;",];
 implementors["std"] = [];
 
             if (window.register_implementors) {
