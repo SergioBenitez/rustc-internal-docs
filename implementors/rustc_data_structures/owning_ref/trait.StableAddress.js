02ed457da87b3a40d1bcea05d1766df933aeb918
Michael Bryan <michaelfbryan@gmail.com>
2017-12-19 00:57:57 +0800

    updated documentation at 2017-12-19 00:57:57.173513052 +08:00

---
+++ implementors/rustc_data_structures/owning_ref/trait.StableAddress.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["rustc_data_structures"] = ["impl&lt;O, H&gt; <a class=\"trait\" href=\"rustc_data_structures/owning_ref/trait.StableAddress.html\" title=\"trait rustc_data_structures::owning_ref::StableAddress\">StableAddress</a> for <a class=\"struct\" href=\"rustc_data_structures/owning_ref/struct.OwningHandle.html\" title=\"struct rustc_data_structures::owning_ref::OwningHandle\">OwningHandle</a>&lt;O, H&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;O: <a class=\"trait\" href=\"rustc_data_structures/owning_ref/trait.StableAddress.html\" title=\"trait rustc_data_structures::owning_ref::StableAddress\">StableAddress</a>,<br>&nbsp;&nbsp;&nbsp;&nbsp;H: <a class=\"trait\" href=\"rustc_data_structures/owning_ref/trait.StableAddress.html\" title=\"trait rustc_data_structures::owning_ref::StableAddress\">StableAddress</a>,&nbsp;</span>","impl&lt;O, T:&nbsp;?<a class=\"trait\" href=\"core/marker/trait.Sized.html\" title=\"trait core::marker::Sized\">Sized</a>&gt; <a class=\"trait\" href=\"rustc_data_structures/owning_ref/trait.StableAddress.html\" title=\"trait rustc_data_structures::owning_ref::StableAddress\">StableAddress</a> for <a class=\"struct\" href=\"rustc_data_structures/owning_ref/struct.OwningRef.html\" title=\"struct rustc_data_structures::owning_ref::OwningRef\">OwningRef</a>&lt;O, T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
