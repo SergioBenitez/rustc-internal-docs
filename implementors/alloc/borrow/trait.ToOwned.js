1fedf58ab5df333019c75c111effe617204f1000
Michael Bryan <michaelfbryan@gmail.com>
2017-12-21 02:02:45 +0800

    updated documentation at 2017-12-21 02:02:45.623437578 +08:00

--- implementors/alloc/borrow/trait.ToOwned.js
+++ implementors/alloc/borrow/trait.ToOwned.js
@@ -1,6 +1,5 @@
 (function() {var implementors = {};
 implementors["alloc"] = [];
-implementors["rustc_data_structures"] = ["impl&lt;T:&nbsp;<a class=\"trait\" href=\"rustc_data_structures/indexed_vec/trait.Idx.html\" title=\"trait rustc_data_structures::indexed_vec::Idx\">Idx</a>&gt; <a class=\"trait\" href=\"alloc/borrow/trait.ToOwned.html\" title=\"trait alloc::borrow::ToOwned\">ToOwned</a> for <a class=\"struct\" href=\"rustc_data_structures/indexed_set/struct.IdxSet.html\" title=\"struct rustc_data_structures::indexed_set::IdxSet\">IdxSet</a>&lt;T&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- implementors/core/borrow/trait.BorrowMut.js
+++ implementors/core/borrow/trait.BorrowMut.js
@@ -1,7 +1,5 @@
 (function() {var implementors = {};
 implementors["core"] = [];
-implementors["rustc_data_structures"] = ["impl&lt;T:&nbsp;<a class=\"trait\" href=\"rustc_data_structures/indexed_vec/trait.Idx.html\" title=\"trait rustc_data_structures::indexed_vec::Idx\">Idx</a>&gt; <a class=\"trait\" href=\"core/borrow/trait.BorrowMut.html\" title=\"trait core::borrow::BorrowMut\">BorrowMut</a>&lt;<a class=\"struct\" href=\"rustc_data_structures/indexed_set/struct.IdxSet.html\" title=\"struct rustc_data_structures::indexed_set::IdxSet\">IdxSet</a>&lt;T&gt;&gt; for <a class=\"struct\" href=\"rustc_data_structures/indexed_set/struct.IdxSetBuf.html\" title=\"struct rustc_data_structures::indexed_set::IdxSetBuf\">IdxSetBuf</a>&lt;T&gt;",];
-implementors["smallvec"] = ["impl&lt;A:&nbsp;<a class=\"trait\" href=\"smallvec/trait.Array.html\" title=\"trait smallvec::Array\">Array</a>&gt; <a class=\"trait\" href=\"core/borrow/trait.BorrowMut.html\" title=\"trait core::borrow::BorrowMut\">BorrowMut</a>&lt;<a class=\"primitive\" href=\"std/primitive.slice.html\">[</a>A::<a class=\"type\" href=\"smallvec/trait.Array.html#associatedtype.Item\" title=\"type smallvec::Array::Item\">Item</a><a class=\"primitive\" href=\"std/primitive.slice.html\">]</a>&gt; for <a class=\"struct\" href=\"smallvec/struct.SmallVec.html\" title=\"struct smallvec::SmallVec\">SmallVec</a>&lt;A&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
